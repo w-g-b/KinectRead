2b3e707cd9d49e8051414ae0692c2a0d69e1ac36
GreenLight <1251191746@qq.com>
2018-06-18 17:00:02 +0800

    Kinect翻书

---
+++ Assets/Scripts/MyPageController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPageController : MonoBehaviour
+{
+    private MyGestureListener gestureListener;
+    public AutoFlip autoFlip;
+
+    private void Start()
+    {
+        // hide mouse cursor
+        Cursor.visible = false;
+
+        // get the gestures listener
+        gestureListener = MyGestureListener.Instance;
+    }
+
+    private void Update()
+    {
+        if (!gestureListener)
+            return;
+
+
+        if (Input.GetKeyDown(KeyCode.PageDown))
+        autoFlip.FlipRightPage();
+        else if (Input.GetKeyDown(KeyCode.PageUp))
+            autoFlip.FlipLeftPage();
+
+        if (gestureListener)
+            if (gestureListener.IsSwipeLeft())
+                autoFlip.FlipRightPage();
+            else if (gestureListener.IsSwipeRight())
+                autoFlip.FlipLeftPage();
+    }
+}
